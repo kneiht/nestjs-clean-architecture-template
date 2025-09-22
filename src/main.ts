bcdd011a1f5895f9504eb745e21217d327aac94c
Thien Nguyen <minhthien.k@gmail.com>
2025-09-23 00:21:20 +0700

    feat(env): add env config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './adapters/nestjs/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { env } from '@/config/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(env.PORT ?? 3000);
 }
 bootstrap();
