42dcd44e3681a2957c3aa7ab00040df8287477e1
Thien Nguyen <minhthien.k@gmail.com>
2025-09-22 22:53:52 +0700

    fix(user-repository): change return to User for findByName

--- src/application/repositories/user.repository.ts
+++ src/application/repositories/user.repository.ts
@@ -3,5 +3,5 @@ import { IBaseRepository } from './base.repository';
 
 export interface IUserRepository extends IBaseRepository<User> {
   findByEmail(email: string): Promise<User | null>;
-  findByName(name: string): Promise<User[] | null>;
+  findByName(name: string): Promise<User | null>;
 }
