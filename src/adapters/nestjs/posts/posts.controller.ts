cec369864086f0b25a8158ddc1a89e2516800906
Thien Nguyen <minhthien.k@gmail.com>
2025-09-22 20:22:50 +0700

    feat(posts): inject GetAllUseCase to posts module and use it in controller

--- src/adapters/nestjs/posts/posts.controller.ts
+++ src/adapters/nestjs/posts/posts.controller.ts
@@ -1,28 +1,12 @@
-import { Body, Controller, Get, Post as HttpPost } from '@nestjs/common';
-import { PostsService } from './posts.service';
-import { CreatePostDto, Post } from '@/entities/post.entity';
+import { Body, Controller, Get } from '@nestjs/common';
+import { GetAllUseCase } from '@/application/use-cases';
 
 @Controller('posts')
 export class PostsController {
-  constructor(private readonly postsService: PostsService) {}
+  constructor(private readonly getAllUseCase: GetAllUseCase) {}
 
   @Get()
-  findAll(): Post[] {
-    return [
-      Post.create({
-        title: 'new title 1',
-        content: 'new content 1',
-      }),
-      Post.create({
-        title: 'new title 2',
-        content: 'new content 2',
-      }),
-    ];
-  }
-
-  @HttpPost()
-  create(@Body() createPostDto: CreatePostDto): Post {
-    const post = Post.create(createPostDto);
-    return post;
+  async findAll() {
+    return await this.getAllUseCase.execute();
   }
 }
--- src/adapters/nestjs/posts/posts.module.ts
+++ src/adapters/nestjs/posts/posts.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
-import { PostsService } from './posts.service';
+
+import { GetAllUseCase } from '@/application/use-cases';
+import { IPostRepository } from '@/application/repositories';
+import { PostInMemoryRepository } from '@/adapters/repositories/in-memory';
 
 @Module({
   controllers: [PostsController],
-  providers: [PostsService]
+  providers: [
+    {
+      provide: GetAllUseCase,
+      useFactory: (postRepository: IPostRepository) => {
+        return new GetAllUseCase(postRepository);
+      },
+      inject: [PostInMemoryRepository],
+    },
+    {
+      provide: PostInMemoryRepository,
+      useClass: PostInMemoryRepository,
+    },
+  ],
 })
 export class PostsModule {}
