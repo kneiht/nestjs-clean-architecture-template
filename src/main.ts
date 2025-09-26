ddd0bfba9d1d671b115d9fc33b6276b0b396c4e5
Thien Nguyen <minhthien.k@gmail.com>
2025-09-26 20:15:07 +0700

    feat(mongodb): add mongodb connection

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './adapters/nestjs/app.module';
 import { UseCaseResponseInterceptor } from './adapters/nestjs/interceptors/response.interceptor';
+
 import { env } from '@/config/environment';
+import { connectDb } from '@/config/database';
 
 async function bootstrap() {
+  await connectDb();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalInterceptors(new UseCaseResponseInterceptor());
