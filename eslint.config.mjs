f1d063c86c21cfe3cdcf8ee53cb37cf45a514bf7
Thien Nguyen <minhthien.k@gmail.com>
2025-09-27 14:23:22 +0700

    chore: update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,7 +27,7 @@ export default [
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-empty-object-type': 'warn',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
